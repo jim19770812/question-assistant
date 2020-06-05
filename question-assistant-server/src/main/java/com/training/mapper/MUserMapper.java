27f59741c9a548837f535f0cf31d7179a9327488
hanxiaofeng <hanxiaofeng77@163.com>
2020-06-05 16:25:12 +0800

    1

--- question-assistant-server/src/main/java/com/training/mapper/MUserMapper.java
+++ question-assistant-server/src/main/java/com/training/mapper/MUserMapper.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import com.training.beans.MUser;
@@ -18,9 +19,9 @@ import com.training.beans.MUser;
 @Component
 public interface MUserMapper extends BaseMapper<MUser> {
     @Select("select * from m_user where usr_email=#{email} and usr_pwd=md5(md5(concat(#{password}, usr_slat)))")
-    MUser findUserByEmailAndPassword(final String email, final String password);
+    MUser findUserByEmailAndPassword(@Param("email")final String email, @Param("password") final String password);
     @Select("select * from m_user where usr_email=#{email}")
-    MUser findUserByEmail(final String email);
+    MUser findUserByEmail(@Param("email")final String email);
     @Select("select count(1) from m_user where usr_email=#{email}")
-    int countUserByEmail(final String email);
+    int countUserByEmail(@Param("email")final String email);
 }
